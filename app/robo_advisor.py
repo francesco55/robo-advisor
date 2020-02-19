63889a839e1d44572f49452ea49496ecf78e1a94
Francesco Zeolla <fdz2@georgetown.edu>
2020-02-19 18:05:25 -0500

    ticker validation, dictionary comprehension for recent data

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -4,6 +4,7 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+import datetime
 
 load_dotenv()
 
@@ -11,7 +12,17 @@ API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
 print("REQUESTING SOME DATA FROM THE INTERNET...")
 
-TICKER = "TSLA"
+TICKER = input("Please enter the company's ticker.")
+#ticker validation
+ticker_max = 4
+if type(TICKER) == str:
+    if len(TICKER) > ticker_max:
+        print("This is not a valid ticker")
+        exit()
+else:
+    print("this is not a valid ticker")
+    exit()
+
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={TICKER}&apikey={API_KEY}"
 print("URL:", request_url)
 
@@ -27,25 +38,29 @@ if "Error Message" in response.text:
 
 parsed_response = json.loads(response.text)
 print(parsed_response)
-breakpoint()
 
 tsd = parsed_response["Time Series (Daily)"]
+date = list(tsd.keys()) #https://stackoverflow.com/questions/30362391/how-do-you-find-the-first-key-in-a-dictionary
+recent_close = tsd[str(date[0])]["4. close"]
+recent_high = tsd[str(date[0])]["2. high"]
+recent_low = tsd[str(date[0])]["3. low"]
 
-for date, prices in tsd.items():
-    print(date)
+
+#for date, prices in tsd.items():
+#    print(date)
 
 
 
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print(f"SELECTED SYMBOL: {TICKER}")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print(f"REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
+print(f"LATEST DAY: 2018-02-20")
+print(f"LATEST CLOSE: {recent_close}")
+print(f"RECENT HIGH: {recent_high}")
+print(f"RECENT LOW: {recent_low}")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
