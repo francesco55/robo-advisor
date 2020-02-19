c4a06d402ba4aafe51bc619ec9d9e1edcf1e8479
Francesco Zeolla <fdz2@georgetown.edu>
2020-02-19 18:53:14 -0500

    list creation for numerical data

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -37,13 +37,40 @@ if "Error Message" in response.text:
 
 
 parsed_response = json.loads(response.text)
-print(parsed_response)
+#print(parsed_response)
 
 tsd = parsed_response["Time Series (Daily)"]
+
 date = list(tsd.keys()) #https://stackoverflow.com/questions/30362391/how-do-you-find-the-first-key-in-a-dictionary
 recent_close = tsd[str(date[0])]["4. close"]
 recent_high = tsd[str(date[0])]["2. high"]
 recent_low = tsd[str(date[0])]["3. low"]
+
+open_p = []
+high = []
+low = []
+close = []
+volume = []
+
+
+for d in date:
+    print(type(d))
+    current_open = float(tsd[d]["1. open"])
+    open_p.append(current_open)
+    current_high = float(tsd[d]["2. high"])
+    high.append(current_high)
+    current_low = float(tsd[d]["3. low"])
+    low.append(current_low)
+    current_close = float(tsd[d]["4. close"])
+    close.append(current_close)
+    current_volume = float(tsd[d]["5. volume"])
+    volume.append(current_volume)
+
+#print(open_p)
+#print(high)
+#print(low)
+#print(close)
+#print(volume)
 
 
 #for date, prices in tsd.items():
